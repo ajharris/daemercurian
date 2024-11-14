a2726bcc723300249251d6db15e974d015330e89
Andrew Harris <Andrew.Harris@torontomu.ca>
2024-11-14 13:21:44 -0500

    serving React frontend

--- backend/app.py
+++ backend/app.py
@@ -1,13 +1,13 @@
 # backend/app.py
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_mail import Mail
 from .config import Config
 
 mail = Mail()  # Initialize mail here globally
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder="../frontend/build", static_url_path="")
     app.config.from_object(Config)
 
     # Initialize mail within the app context
@@ -16,5 +16,14 @@ def create_app():
     # Register blueprints
     from backend.routes.email_routes import email_routes
     app.register_blueprint(email_routes)
+
+    # Serve React App
+    @app.route("/")
+    @app.route("/<path:path>")
+    def serve_react_app(path=""):
+        if path != "" and os.path.exists(app.static_folder + "/" + path):
+            return send_from_directory(app.static_folder, path)
+        else:
+            return send_from_directory(app.static_folder, "index.html")
 
     return app
